50c3ad86f3f731de4dabb1564b49105718344657
rod martin <rod@araknia.com>
2022-03-15 11:29:44 +0000

    show what code does

--- Assets/Scripts/EnemyTargeting.cs
+++ Assets/Scripts/EnemyTargeting.cs
@@ -1,39 +1,35 @@
-﻿using System;
-using MathHelpers;
+﻿using MathHelpers;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-namespace DefaultNamespace
+public class EnemyTargeting : MonoBehaviour
 {
-    public class EnemyTargeting : MonoBehaviour
+    public Transform bulllet;
+    public Transform gunBarrel;
+    public float bulletVelocity = 5f;
+    public GameObject target;
+    public float speedRotation = 9f;
+    private int shootDelay = 90;
+    private Rigidbody target_rb;
+
+    private void Start()
     {
-        public Transform bulllet;
-        public Transform gunBarrel;
-        public float bulletVelocity = 5f;
-        public GameObject target;
-        public float speedRotation = 9f;
-        private int shootDelay = 90;
-        private Rigidbody target_rb;
-        private void Start()
-        {
-            target_rb = target.GetComponent<Rigidbody>();
-        }
-        private void Update()
-        {
+        target_rb = target.GetComponent<Rigidbody>();
+    }
 
-            Vector3 aimLoc = Targeting.GetMovingTarget(transform.position, bulletVelocity, target.transform.position,target_rb.velocity);
-           
-            Targeting.RotateToTarget(transform, aimLoc, speedRotation);
-            if (Time.frameCount % shootDelay == 0)
-            {
-               Transform b = Instantiate(bulllet, gunBarrel.position, gunBarrel.rotation);
-               Rigidbody brb = b.GetComponent<Rigidbody>();
-               brb.AddForce(transform.forward * bulletVelocity, ForceMode.Impulse);
-               Destroy(b.gameObject, 3f);
-            }
-            
+    private void Update()
+    {
+        // A: get target prediction
+        Vector3 aimLoc = Targeting.GetMovingTarget(transform.position, bulletVelocity, target.transform.position,
+            target_rb.velocity);
+        // B: Rotate transform to target
+        Targeting.RotateToTarget(transform, aimLoc, speedRotation);
+        // C: shoot a missile at target
+        if (Time.frameCount % shootDelay == 0)
+        {
+            Transform b = Instantiate(bulllet, gunBarrel.position, gunBarrel.rotation);
+            Rigidbody brb = b.GetComponent<Rigidbody>();
+            brb.AddForce(transform.forward * bulletVelocity, ForceMode.Impulse);
+            Destroy(b.gameObject, 3f);
         }
-
-       
     }
 }
